0227870edf6e0af4e78cbdd46325f76562c51914
ghena_Die <empirs@gmail.com>
2019-05-14 20:55:45 +0200

    Actualització responsable.

--- src/main/webapp/responsable/responsable.controller.js
+++ src/main/webapp/responsable/responsable.controller.js
@@ -1,26 +1,68 @@
-app.controller("PageController", function ($scope, $http, $window,$rootScope, $location, Auth) {
-   
-    console.log("PageController initialized");
-    
-    $scope.Pages = [
-        {icon: "fas fa-layer-group", name:"Inventari", url:"#!inventari"},
-        {icon: "fas fa-shopping-basket", name:"Comandes", url:"#!comandes"},
-        {icon: "fas fa-file", name:"Informes", url:"#!informes"}
-    ]
-               
-    this.$onInit = onInit();
-    
-    function onInit() {
-            $('#menuCollapse').on('click', function () {
-                $('#sidebar').toggleClass('active');
-            });
-        
-    }
-           
-      $rootScope.logout = function(){
-        Auth.logout();
-                $window.location.href = "../";
-      };
-    
-});
+app.controller("PageController", function ($scope, $http, $window, $rootScope, $location, Auth, Notification) {
 
+    console.log("PageController initialized");
+
+    $scope.Pages = [
+        {
+            icon: "fas fa-layer-group",
+            name: "Inventari",
+            url: "#!inventari"
+        },
+        {
+            icon: "fas fa-shopping-basket",
+            name: "Comandes",
+            url: "#!comandes"
+        },
+        {
+            icon: "fas fa-file",
+            name: "Informes",
+            url: "#!informes"
+        }
+    ]
+
+    this.$onInit = onInit();
+
+    function onInit() {
+        $('#menuCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
+        });
+
+    }
+    
+    $scope.exportToExcel = function(tableId, nomInforme) {        
+        var fileName = 'Informe' + nomInforme + '_' + Auth.currentUser().botiga.nom + '_' + moment().format("DD-MM-YYYY") + "_Setmana_" + moment().isoWeek();
+        alasql("SELECT * INTO XLSX('" + fileName + ".xlsx',{headers:true}) \
+                    FROM HTML('" + tableId + "',{headers:true})");
+        Notification.primary("Informe Excel exportat!");
+    }
+    
+    $scope.exportToCsv = function(tableId, nomInforme) {                 
+        var fileName = 'Informe' + nomInforme + '_' + Auth.currentUser().botiga.nom + '_' + moment().format("DD-MM-YYYY") + "_Setmana_" + moment().isoWeek() + '.csv';
+		$(tableId).tableHTMLExport({
+		  type:'csv',
+		  filename:fileName
+		});
+        Notification.primary("Informe CSV exportat!");
+    }
+    
+    $scope.exportToPdf = function(tableId, nomInforme) {        
+        var fileName = 'Informe' + nomInforme + '_' + Auth.currentUser().botiga.nom + '_' + moment().format("DD-MM-YYYY") + "_Setmana_" + moment().isoWeek();
+		$(tableId).tableHTMLExport({
+		  type:'pdf',
+		  filename:fileName
+		});
+        Notification.primary("Informe PDF exportat!");
+    }
+    
+    
+    
+
+    $rootScope.logout = function () {
+        Auth.logout();
+        $window.location.href = "../";
+    };
+
+
+
+
+});
